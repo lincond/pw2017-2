7888722ddaeac495ee48cc5554f52f5043084782
Lincon Dias <lincondiasribeiro@gmail.com>
2017-09-15 20:05:31 -0300

    Graph now can change color based on IMC condition

--- imc/js/imc.js
+++ imc/js/imc.js
@@ -1,12 +1,12 @@
-$("#peso").on("change", function(){
+$("#peso").on("change focus focusin focusout click keyup", function(){
     calcula();
 });
 
-$("#altura").on("change", function(){
+$("#altura").on("change focus focusin focusout click keyup", function(){
     calcula();
 });
 
-$("#sexo").on("change", function(){
+$("#sexo").on("change focus focusin focusout click", function(){
     calcula();
 });
 
@@ -22,32 +22,62 @@ function calcula()
 
     var imc = peso / (altura * altura);
 
+    var color = "#ffc642";
+    var condicao = "Normal";
+
     if ( sexo == "Masculino")
     {
-        if (imc < 20.7)
-            $(".condicao").html("Abaixo do peso");
-        else if( imc >= 20.7 && imc < 26.4 )
-            $(".condicao").html("Normal");
-        else if( imc >= 26.4 && imc < 27.8 )
-            $(".condicao").html("Pouco acima do peso");
-        else if( imc >= 27.8 && imc < 31.1 )
-            $(".condicao").html("Acima do peso");
-        else
-            $(".condicao").html("Obeso");
+        if (imc < 20.7) {
+            color = "#ffc642";
+            condicao = "Abaixo do peso";
+        }
+        else if( imc >= 20.7 && imc < 26.4 ) {
+            color = "#66CC66";
+            condicao = "Normal";
+        }
+        else if( imc >= 26.4 && imc < 27.8 ) {
+            color = "#ffe241";
+            condicao = "Pouco acima do peso";
+        }
+        else if( imc >= 27.8 && imc < 31.1 ) {
+            color = "#ffc642";
+            condicao = "Acima do peso";
+        }
+        else {
+            color = "#ff4141";
+            condicao = "Obeso";
+        }
     }
     else
     {
-        if (imc < 19.1)
-            $(".condicao").html("Abaixo do peso");
-        else if( imc >= 19.1 && imc < 25.8 )
-            $(".condicao").html("Normal");
-        else if( imc >= 25.8 && imc < 27.3 )
-            $(".condicao").html("Pouco acima do peso");
-        else if( imc >= 27.3 && imc < 32.3 )
-            $(".condicao").html("Acima do peso");
+        if (imc < 19.1) {
+            color = "#ffc642";
+            condicao = "Abaixo do peso";
+        }
+        else if( imc >= 19.1 && imc < 25.8 ) {
+            color = "#66CC66";
+            condicao = "Normal";
+        }
+        else if( imc >= 25.8 && imc < 27.3 ) {
+            color = "#ffe241";
+            condicao = "Pouco acima do peso";
+        }
+        else if( imc >= 27.3 && imc < 32.3 ){
+            color = "#ffc642";
+            condicao = "Acima do peso";
+        }
         else
-            $(".condicao").html("Obeso");
+        {
+            color = "#ff4141";
+            condicao = "Obeso";
+        }
     }
 
-    $("#imc").val(imc.toFixed(2)).trigger('change');
+    // Altera o CSS e o gráfico
+    $(".condicao").html(condicao);
+    $(".condicao").css('color', color);
+    $("#imc").val(imc.toFixed(2)).trigger('change').trigger('configure',{
+        'fgColor': color,
+        'inputColor': color
+    });
 }
